cb07acbed07d57b9cf515b7eacd25c704125d99d
Alexander Thompson <alexthompson429@gmail.com>
2020-11-24 10:03:27 -0700

    API searh attribute

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ let dd = String(today.getDate()).padStart(2, '0');
 let mm = String(today.getMonth() + 1).padStart(2, '0');
 let yyyy = today.getFullYear();
 var today = mm + '/' + dd + '/' + yyyy;
+
+
+let apiKey = "3ac0d8db34de82819d13a9167239acc1";
+let searchBtn = $(".searchBtn");
+let searchInput = $(".searchInput");
